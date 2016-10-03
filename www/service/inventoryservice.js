4325af85d94600936fa91f3ee6dd1141672c4b1f
RameshMalla <ramesh.malla6@gmail.com>
2016-10-04 01:36:48 +0530

    few modifications

--- www/service/inventoryservice.js
+++ www/service/inventoryservice.js
@@ -1,9 +1,12 @@
 expressPayModule.service("inventoryService", function($http) {
   var shoppingList = [];
+  var totalprice = 0;
   this.setShoppingList = function(storeId, itemId, callbackFunction) {
-    $http.get("http://localhost:3000/inventory/getItemById/"+storeId+"/"+itemId)
+    $http.get("http://localhost:3000/inventory/getItemById/" + storeId + "/" + itemId)
       .then(function(response) {
         shoppingList.push(response.data);
+        totalprice = totalprice + Number(response.data.itemPrice);
+        //console.log(totalprice);
         callbackFunction(response.data);
       }, function(response) {
         callbackFunction(response.data);
@@ -12,5 +15,8 @@ expressPayModule.service("inventoryService", function($http) {
   }
   this.getShoppinList = function() {
     return shoppingList;
+  }
+  this.getTotalPrice = function() {
+    return totalprice;
   }
 });
