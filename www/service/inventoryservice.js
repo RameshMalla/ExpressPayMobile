d9890c25e3cbdca92b3421d1bac992d9388bc3d3
RameshMalla <ramesh.malla6@gmail.com>
2016-10-05 00:24:56 +0530

    few changes

--- www/service/inventoryservice.js
+++ www/service/inventoryservice.js
@@ -1,53 +1,63 @@
 expressPayModule.service("inventoryService", function($http) {
-    var shoppingList = [];
-    var totalprice = 0;
-    var totalCartItems = 0;
-    this.setShoppingList = function(storeId, itemId, callbackFunction) {
+  var shoppingList = [];
+  var totalprice = 0;
+  var totalCartItems = 0;
+  this.setShoppingList = function(storeId, itemId, callbackFunction) {
 
-        var isSameItemAdded = false;
+    var isSameItemAdded = false;
 
-        if (shoppingList.length > 0) {
-            angular.forEach(shoppingList, function(value, key) {
-                if (value.storeId == storeId && value.itemId == itemId) {
-                    totalCartItems = totalCartItems + 1;
-                    totalprice = totalprice + Number(value.itemPrice);
-                    value.totalQuantity = value.totalQuantity + 1;
-                    isSameItemAdded = true;
-                    callbackFunction(shoppingList);
-                    return false;
-                }
-            });
+    if (shoppingList.length > 0) {
+      angular.forEach(shoppingList, function(value, key) {
+        if (value.storeId == storeId && value.itemId == itemId) {
+          totalCartItems = totalCartItems + 1;
+          totalprice = totalprice + Number(value.itemPrice);
+          value.totalQuantity = value.totalQuantity + 1;
+          isSameItemAdded = true;
+          callbackFunction(shoppingList);
+          return false;
         }
-        if (!isSameItemAdded) {
-            console.log("Called");
-            $http.get("http://localhost:3000/inventory/getItemById/" + storeId + "/" + itemId)
-                .then(function(response) {
-                    totalCartItems = totalCartItems + 1;
-                    totalprice = totalprice + Number(response.data.itemPrice);
-                    shoppingList.push(response.data);
-                    callbackFunction(response.data);
-                }, function(response) {
-                    callbackFunction(response.data);
-                });
-        }
+      });
     }
-    this.getShoppinList = function() {
-        return shoppingList;
+    if (!isSameItemAdded) {
+      console.log("Called");
+      $http.get("http://localhost:3000/inventory/getItemById/" + storeId + "/" + itemId)
+        .then(function(response) {
+          totalCartItems = totalCartItems + 1;
+          totalprice = totalprice + Number(response.data.itemPrice);
+          shoppingList.push(response.data);
+          callbackFunction(response.data);
+        }, function(response) {
+          callbackFunction(response.data);
+        });
     }
-    this.getTotalPrice = function() {
-        return totalprice;
-    }
-    this.getTotalCartItems = function() {
-        return totalCartItems;
+  }
+  this.getShoppinList = function() {
+    return shoppingList;
+  }
+  this.getTotalPrice = function() {
+    return totalprice;
+  }
+  this.getTotalCartItems = function() {
+    return totalCartItems;
+  }
+
+  this.updateTotalPrice = function(modifiedPrice, isReduced) {
+    if (isReduced) {
+      totalprice = totalprice - modifiedPrice;
+    } else {
+      totalprice = modifiedPrice;
     }
 
-    this.updateTotalPrice = function(modifiedPrice) {
-        totalprice = totalprice - modifiedPrice;
-        return totalprice;
+    return totalprice;
+  }
+
+  this.updateTotalCartItems = function(modifiedTotalCart, isReduced) {
+    if (isReduced) {
+      totalCartItems = totalCartItems - modifiedTotalCart;
+    } else {
+      totalCartItems = totalCartItems + modifiedTotalCart;
     }
 
-    this.updateTotalCartItems = function(modifiedTotalCart) {
-        totalCartItems = totalCartItems - modifiedTotalCart;
-        return totalCartItems;
-    }
+    return totalCartItems;
+  }
 });
